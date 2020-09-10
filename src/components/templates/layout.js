19b36e76470f91f96e911a5202df0f6047afb633
Navdeep Singh Rathore <navdeepsinghrathore2000@gmail.com>
2020-09-10 16:55:25 +0530

    added files

---
+++ src/components/templates/layout.js
@@ -0,0 +1,34 @@
+import React from "react"
+import Header from '../header';
+import Footer from '../footer';
+
+import {graphql, useStaticQuery} from "gatsby";
+
+import '../../styles/index.scss'
+import layoutStyle from '../component-styles/layout.module.scss';
+
+export default function Layout(props) {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+          author
+        }
+      }
+    }
+  `)
+  return (
+    <div className={layoutStyle.root}>
+      <div className={layoutStyle.outerContainer}>
+        <div className={layoutStyle.container}>
+          <div id="content" className={layoutStyle.content}>
+            <Header title={data.site.siteMetadata.title}/>
+            {props.children}
+          </div>
+          <Footer author={data.site.siteMetadata.author}/>
+        </div>
+      </div>
+    </div>
+  )
+}
