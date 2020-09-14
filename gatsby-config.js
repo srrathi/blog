1c9bbbb2e668a037c42c5f2354318150661f2d6c
Navdeep Singh Rathore <navdeepsinghrathore2000@gmail.com>
2020-09-14 06:50:53 +0530

    Restoring

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,6 +52,5 @@ module.exports = {
 
         }
     },
-    `gatsby-plugin-react-helmet`
   ],
 }
--- src/components/templates/blogPost.js
+++ src/components/templates/blogPost.js
@@ -2,7 +2,6 @@ import React from "react";
 import { graphql} from "gatsby";
 
 import Layout from './layout';
-import Head from '../head'
 import blogPostStyles from '../component-styles/blogPost.module.scss';
 
 export const query = graphql`
@@ -31,7 +30,6 @@ const BlogPost = (props) => {
   // console.log(props.data)
   return(
     <Layout>
-      <Head title={props.data.markdownRemark.frontmatter.title}/>
       <div className={blogPostStyles.header}>
         <h1 className={blogPostStyles.title}>{props.data.markdownRemark.frontmatter.title}</h1>
         <ol className={blogPostStyles.tagList}>
